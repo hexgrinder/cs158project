e14879211149fed4702eb331f5fce1b2f961beb4
hexgrinder <anon@anon.com>
2015-04-05 15:52:51 -0700

    Hello world test

---
+++ src/cs158project/HelloWorld.java
@@ -0,0 +1,13 @@
+package cs158project;
+
+/**
+ * Hello world example
+ * @author M Layman
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
