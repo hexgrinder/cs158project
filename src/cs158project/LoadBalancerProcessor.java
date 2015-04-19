bfceb71a9e6d045f1544184d0780d1fe72110fb0
hexgrinder <anon@anon.com>
2015-04-19 11:44:52 -0700

    Continued test and debugging.

--- src/cs158project/LoadBalancerProcessor.java
+++ src/cs158project/LoadBalancerProcessor.java
@@ -6,8 +6,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.UnknownHostException;
 import java.util.Random;
 import java.util.UUID;
@@ -53,13 +59,14 @@ public class LoadBalancerProcessor implements Runnable {
 			//10.10.10.3
 			//port 80
 */
-			ML_printToConsole_(request_);
+			//ML_printToConsole_(request_);
+			ML_process_(request_);
 			
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
+			System.out.println("[DEBUG] Unknown host exception.");
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.out.println("[DEBUG] IO exception.");
 			e.printStackTrace();
 		} finally {
 			try {
@@ -81,6 +88,13 @@ public class LoadBalancerProcessor implements Runnable {
 	 */
 	private void ML_printToConsole_(Socket sock) throws IOException {
 		
+		// retreives sources address and port
+		System.out.println(
+			String.format(
+				"[DEBUG] source port: %d, source address: %s", 
+				sock.getPort(),
+				sock.getInetAddress()));
+		
 		BufferedReader inbufread = new BufferedReader(
 			new InputStreamReader(sock.getInputStream()));
 		
@@ -89,28 +103,143 @@ public class LoadBalancerProcessor implements Runnable {
 			System.out.println(str);
 		}
 		
+		System.out.println("[DEBUG] Closing in buffer.");
+		
 		inbufread.close();
+	}
+	
+	private void ML_makeConnection(Socket inbound) {
+		
+		// query protocol for destination
+		ConnectionConfiguration destination = protocol_.getResource();
+		
+		System.out.println(
+			String.format(
+				"[DEBUG] Trying to connect to outbound host: %s, port %d.",
+				destination.host,
+				destination.port));
+		
+		BufferedReader destinbufread = null;
+		OutputStreamWriter inwrite = null;
+		
+		try {
+			
+			// open an http connection to the destination
+			URL dest = new URL(destination.host);
+			HttpURLConnection desthttp = (HttpURLConnection) dest.openConnection();
+			
+			// setup input from dest
+			destinbufread = new BufferedReader(
+				new InputStreamReader(desthttp.getInputStream()));
+			
+			// setup output to inbound
+			inwrite = new OutputStreamWriter(
+				inbound.getOutputStream());	
+				
+			// write to inbound
+			
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (null != destinbufread) {
+				try {
+					destinbufread.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (null != inwrite) {
+				try {
+					inwrite.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
 	}
 	
 	private void ML_process_(Socket inbound) throws UnknownHostException, IOException {
 		
 		// query protocol for destination
 		ConnectionConfiguration destination = protocol_.getResource();
-				
+		
+		System.out.println(
+			String.format(
+				"[DEBUG] Trying to connect to outbound host: %s, port %d.",
+				destination.host,
+				destination.port));
+/*
+		OutputStreamWriter osw = new OutputStreamWriter(
+			inbound.getOutputStream());
+		osw.write("You have hit the load balancer.");
+		osw.close();
+*/
+		
+		// note: creating a new socket will open a connection
 		Socket outbound = new Socket(destination.host, destination.port);
 		
-		BufferedReader inbufread = new BufferedReader(
-			new InputStreamReader(inbound.getInputStream()));
+		if (outbound.isConnected()) {
+			System.out.println(
+				String.format(
+					"[DEBUG] Connected outbound: %s.", 
+					outbound.getRemoteSocketAddress()));
 			
-		OutputStreamWriter osw = new OutputStreamWriter(
-			outbound.getOutputStream());
+			BufferedReader inbufread = new BufferedReader(
+				new InputStreamReader(inbound.getInputStream()));
+			OutputStreamWriter inwrite = new OutputStreamWriter(
+				inbound.getOutputStream());	
+			
+			BufferedReader outbufread = new BufferedReader(
+				new InputStreamReader(outbound.getInputStream()));
+			OutputStreamWriter outwrite = new OutputStreamWriter(
+				outbound.getOutputStream());
 		
-		String val;
-		while ((val = inbufread.readLine()) != null) {
-			osw.write(val);
-			osw.flush();
+			String val;
+			// BUG: read is blocking
+			// BUG: halts after header section -> it hits a 1013 set then stops
+
+			while ((val = inbufread.readLine()) != null) {
+				// HOW TO DEAL WITH BREAK?
+				if ("\n" != val) {
+					// inbound -> outbound
+					System.out.println(
+						String.format("[DEBUG PROCESS IN->OUT] %s", val));
+					outwrite.write(String.format("%s\n", val));
+					outwrite.flush();
+				} else {
+					System.out.println("[DEBUG PROCESS IN->OUT] HALT");
+				}
+			}
+// uncomment the following to send response from server to client
+/*
+			
+			while ((val = outbufread.readLine()) != null) {
+				// outbound -> inbound
+				System.out.println(
+					String.format("[DEBUG PROCESS OUT->IN] %s", val));
+				inwrite.write(val);
+				inwrite.flush();
+			}
+*/
+			inbufread.close();
+			outbufread.close();
+			inwrite.close();
+			outwrite.close();
+			
+			
+		} else {
+			System.out.println("[DEBUG] Outbound not connected.");
 		}
 		
+		System.out.println("[DEBUG] Closing outbound.");
 		outbound.close();
 	}
 
